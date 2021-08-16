d7191db321b9f9b9839f5070771e0f2540330ecc
momo427 <ferguson.tyra@gmail.com>
2021-08-16 10:13:55 -0400

    working on frontend

--- 02-Homework/Develop/client/src/pages/SavedBooks.js
+++ 02-Homework/Develop/client/src/pages/SavedBooks.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
 
-import { getMe, deleteBook } from '../utils/API';
+import {QUERY_BOOKS, QUERY_ME } from '../utils/queries';
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
 
@@ -20,7 +20,7 @@ const SavedBooks = () => {
           return false;
         }
 
-        const response = await getMe(token);
+        const response = await QUERY_ME(token);
 
         if (!response.ok) {
           throw new Error('something went wrong!');
