32c9e1756545991c5f62543978764d56b224ccae
momo427 <ferguson.tyra@gmail.com>
2021-08-21 14:09:57 -0400

    fix graphql error

--- client/src/App.js
+++ client/src/App.js
@@ -49,8 +49,3 @@ function App() {
 }
 
 export default App;
-<Switch>
-<Route exact path='/' component={SearchBooks} />
-<Route exact path='/saved' component={SavedBooks} />
-<Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
-</Switch>
